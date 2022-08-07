6f39589f31888cdcd76dd40fbdc67268194b20f4
Jonas Dreßler <verdre@v0yd.nl>
2022-08-07 15:11:49 +0200

    tests/clutter/conform: Add more tests for gestures
    Now that we have a few real gesture implementations, we can add more
    tests:
    
    - Add a basic test to make sure events are delivered in the correct
    order

--- src/tests/clutter/conform/gesture.c
+++ src/tests/clutter/conform/gesture.c
@@ -140,8 +140,56 @@ gesture_state_machine_move_to_waiting (void)
   g_signal_handlers_disconnect_by_func (stage, on_presented, &was_presented);
 }
 
+static void
+gesture_event_order (void)
+{
+  ClutterActor *stage = clutter_test_get_stage ();
+  ClutterGesture *gesture_1 = CLUTTER_GESTURE (g_object_new (CLUTTER_TYPE_PAN_GESTURE, "name", "pan-gesture-1", "begin-threshold", 0, NULL));
+  ClutterGesture *gesture_2 = CLUTTER_GESTURE (g_object_new (CLUTTER_TYPE_PAN_GESTURE, "name", "pan-gesture-2", "begin-threshold", 0, NULL));
+  ClutterGesture *gesture_3 = CLUTTER_GESTURE (g_object_new (CLUTTER_TYPE_PAN_GESTURE, "name", "pan-gesture-3", "begin-threshold", 0, NULL));
+  gboolean was_presented = FALSE;
+
+  g_signal_connect (stage, "presented", G_CALLBACK (on_presented),
+                    &was_presented);
+
+  ClutterActor *second_actor = clutter_actor_new ();
+
+  clutter_actor_set_size (second_actor, 200, 200);
+  clutter_actor_set_reactive (second_actor, true);
+  clutter_actor_add_child (stage, second_actor);
+
+  clutter_actor_show (stage);
+
+  was_presented = FALSE;
+  while (!was_presented)
+    g_main_context_iteration (NULL, FALSE);
+
+  clutter_actor_add_action (stage, CLUTTER_ACTION (gesture_1));
+  clutter_actor_add_action (stage, CLUTTER_ACTION (gesture_2));
+  clutter_actor_add_action (second_actor, CLUTTER_ACTION (gesture_3));
+
+  emit_event_and_wait (CLUTTER_STAGE (stage), &was_presented, CLUTTER_BUTTON_PRESS, 15, 15);
+
+  g_assert_true (clutter_gesture_get_state (gesture_1) == CLUTTER_GESTURE_STATE_CANCELLED);
+  g_assert_true (clutter_gesture_get_state (gesture_2) == CLUTTER_GESTURE_STATE_CANCELLED);
+  g_assert_true (clutter_gesture_get_state (gesture_3) == CLUTTER_GESTURE_STATE_RECOGNIZING);
+
+  emit_event_and_wait (CLUTTER_STAGE (stage), &was_presented, CLUTTER_BUTTON_RELEASE, 15, 15);
+
+  g_assert_true (clutter_gesture_get_state (gesture_1) == CLUTTER_GESTURE_STATE_WAITING);
+  g_assert_true (clutter_gesture_get_state (gesture_2) == CLUTTER_GESTURE_STATE_WAITING);
+  g_assert_true (clutter_gesture_get_state (gesture_3) == CLUTTER_GESTURE_STATE_WAITING);
+
+  clutter_actor_destroy (second_actor);
+
+  clutter_actor_remove_action (stage, CLUTTER_ACTION (gesture_1));
+  clutter_actor_remove_action (stage, CLUTTER_ACTION (gesture_2));
+  g_signal_handlers_disconnect_by_func (stage, on_presented, &was_presented);
+}
+
 CLUTTER_TEST_SUITE (
   CLUTTER_TEST_UNIT ("/gesture/disposed-while-active", gesture_disposed_while_active);
   CLUTTER_TEST_UNIT ("/gesture/freed-despite-relationship", gesture_freed_despite_relationship);
   CLUTTER_TEST_UNIT ("/gesture/state-machine-move-to-waiting", gesture_state_machine_move_to_waiting);
+  CLUTTER_TEST_UNIT ("/gesture/event-order", gesture_event_order);
 )
