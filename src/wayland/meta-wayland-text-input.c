8a6600eead8a03a369dbb7495772e5bd17189580
Jonas Dreßler <verdre@v0yd.nl>
2022-08-27 14:51:43 +0200

    Revert "wayland: Only enable OSK if receiving .enable when already active"
    This reverts commit eabb789381f6a2625b37d785b14f2373d529a21b.

--- src/wayland/meta-wayland-text-input.c
+++ src/wayland/meta-wayland-text-input.c
@@ -575,7 +575,7 @@ text_input_commit_state (struct wl_client   *client,
 {
   MetaWaylandTextInput *text_input = wl_resource_get_user_data (resource);
   ClutterInputFocus *focus = text_input->input_focus;
-  gboolean enable_panel = FALSE;
+  gboolean toggle_panel = FALSE;
 
   increment_serial (text_input, resource);
 
@@ -597,12 +597,9 @@ text_input_commit_state (struct wl_client   *client,
               else
                 return;
             }
-          else
-            {
-              enable_panel = TRUE;
-            }
 
           clutter_input_focus_set_can_show_preedit (focus, TRUE);
+          toggle_panel = TRUE;
         }
       else if (clutter_input_focus_is_focused (focus))
         {
@@ -655,8 +652,8 @@ text_input_commit_state (struct wl_client   *client,
 
   meta_wayland_text_input_reset (text_input);
 
-  if (enable_panel)
-    clutter_input_focus_set_input_panel_state (focus, CLUTTER_INPUT_PANEL_STATE_ON);
+  if (toggle_panel)
+    clutter_input_focus_set_input_panel_state (focus, CLUTTER_INPUT_PANEL_STATE_TOGGLE);
 
   meta_wayland_text_input_focus_defer_done (focus);
 }
